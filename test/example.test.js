c8e14ae1b89428a110bca4d1a7c8149123613549
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-10 14:19:26 -0700

    testing

--- test/example.test.js
+++ test/example.test.js
@@ -9,12 +9,12 @@ test('testing renderComposer', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     
-    const expected2 = `<div><h2>J.S Bach</h2><img src="../assets/alchemy-logo.png"><p>Baroque</p><ul>Hits:<li>Cello Suite in G Major: BWV 1007</li><li>Brandenburg Concerto No. 3: BWV 1048</li><li>Prelude No. 1 in C Major: BWV 846</li></ul></div>`;
+    const expected2 = `<div class="composers2"><h2>Johann Sebastian Bach</h2><img src="../assets/bach.jpeg"><p>Baroque</p><ul>Hits:<li>Cello Suite in G Major: BWV 1007</li><li>Brandenburg Concerto No. 3: BWV 1048</li><li>Prelude No. 1 in C Major: BWV 846</li></ul></div>`;
     //Act 
     // Call the function you're testing and set the result to a const
     const actual2 = renderComposerNamesDetails(composersArray[0]);
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual2.outerHTML, expected2);
-   
+    
 });
